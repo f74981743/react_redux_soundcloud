c0c8cf90d738efdf5767c0d82ebedcc02fb3ac23
f74981743 <bewitchsky@gmail.com>
2017-09-01 15:08:12 +0800

    upgrade to webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,13 @@
 const webpack = require('webpack');
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const VENDOR_LIBS = [
-  'react', 'react-dom', 'react-redux', 'react-router',
-  'react-router-redux', 'redux', 'redux-logger', 'redux-thunk',
-  'soundcloud', 'classnames', 'svg-url-loader', 'style-loader', 'lodash', 
-  'babel-preset-stage-1'
-];
 
 module.exports = {
   entry: {
     bundle: './src/index.js',
-    vendor: VENDOR_LIBS
   },
   output: {
     path: path.join(__dirname, 'public'),
-    filename: '[name].[chunkhash].js',
+    filename: 'bundle.js',
     publicPath: './public/'
   },
   module: {
@@ -58,21 +49,6 @@ module.exports = {
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
       }
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: ['vendor', 'manifest'],
-
-      // filename: "vendor.js"
-      // (Give the chunk a different name)
-
-      minChunks: 2,
-      // (with more entries, this ensures that no other module
-      //  goes into the vendor chunk)
-    }),
-    new HtmlWebpackPlugin({
-      title: 'SoundsGood',
-      template: './src/index.html',
-      filename: '../index.html'
     })
   ]
 };
