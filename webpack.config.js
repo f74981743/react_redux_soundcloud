26c89f8c323cf28e31c740ca6c635064bd7f1708
f74981743 <bewitchsky@gmail.com>
2017-09-01 14:29:40 +0800

    upgrade to webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,50 @@
 const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = [
+  'react', 'react-dom', 'react-redux', 'react-router',
+  'react-router-redux', 'redux', 'redux-logger', 'redux-thunk',
+  'soundcloud', 'classnames', 'svg-url-loader', 'style-loader', 'lodash', 
+  'babel-preset-stage-1'
+];
 
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'public'),
-    filename: 'bundle.js',
-    publicPath: '/public/'
+    filename: '[name].[chunkhash].js',
+    publicPath: './public/'
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+    rules: [
+      {
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ["react", "es2015", "stage-1"]
+          }
+        },
+        test: /\.js$/,
+        exclude: /node_modules/,
+      }, {
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+        test: /\.scss$/
+      }, {
+        use: 'svg-url-loader',
+        test: /\.svg/
       }
-    }, {
-      test: /\.scss$/,
-      loader: 'style!css!sass'
-    },{
-      test: /\.(mp3|ogg)$/i, loader: "file?name=[path][name].[ext]"
-    }, {
-      test: /\.svg/, loader: 'svg-url-loader'
-    }]
+    ]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['.js', '.jsx']
   },
   devServer: {
     historyApiFallback: true,
@@ -38,9 +55,24 @@ module.exports = {
       'fetch': 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch'
     }),
     new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
-  })
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['vendor', 'manifest'],
+
+      // filename: "vendor.js"
+      // (Give the chunk a different name)
+
+      minChunks: 2,
+      // (with more entries, this ensures that no other module
+      //  goes into the vendor chunk)
+    }),
+    new HtmlWebpackPlugin({
+      title: 'SoundsGood',
+      template: './src/index.html',
+      filename: '../index.html'
+    })
   ]
 };
